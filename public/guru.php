eefdb858584f491b97178f98506566d7597eb391
Eriri-Spencer20 <radityaappj23@gmail.com>
2021-09-24 08:40:55 +0700

    menampilkan data guru

---
+++ public/guru.php
@@ -0,0 +1,164 @@
+<?php
+include "koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+
+<body>
+    <div class="relative min-h-screen md:flex">
+        <!-- mobile menu box -->
+        <div class="bg-gray-800 text-gray-100 flex justify-between md:hidden">
+            <!-- logo -->
+            <a href="" class="block p-4 text-white font-bold">Life is Better</a>
+
+            <!-- mobile menu button-->
+            <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
+                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </button>
+        </div>
+        <!-- side bar -->
+        <div
+            class="sidebar bg-gray-300 text-gray-700 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0  transition duration-200 ease-in-out">
+            <!-- logo -->
+            <a href="" class="text-indigo-500 flex items-center justify-between px-4">
+                <span class="text-2xl font-bold uppercase tracking-widest">nicfo</span>
+                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                </svg>
+            </a>
+            <!-- nav -->
+            <nav :class="{'flex': open, 'hidden': !open}">
+                <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                    <button @click="open = !open"
+                        class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-center bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-indigo-400 focus:bg-indigo-400 focus:outline-none focus:shadow-outline">
+                        <span>Data Kepegawaian</span>
+                        <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}"
+                            class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                        <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg  md:mt-0 hover:text-white focus:text-white hover:bg-indigo-400 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                href="#">Data Guru</a>
+                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:text-white focus:text-white hover:bg-indigo-400 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                href="#">Data Staff</a>
+                        </div>
+                    </div>
+                </div>
+                <a href=""
+                    class="mt-4 block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-400 hover:text-white text-center">About</a>
+            </nav>
+        </div>
+
+        <!-- content -->
+
+        <!-- component -->
+
+        <div class="flex flex-col w-full p-8">
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-indigo-500 uppercase tracking-wider">
+                                        Name
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-indigo-500 uppercase tracking-wider">
+                                        Job
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-indigo-500 uppercase tracking-wider">
+                                        Birth
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-indigo-500 whitespace-nowrap uppercase tracking-wider">
+                                        Kontak
+                                    </th>
+                                    <th scope="col" class="relative px-6 py-3">
+                                        <span class="sr-only">Details</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <?php
+
+                            $data = mysqli_query($conn, "select * from data_guru");
+                            while ($d = mysqli_fetch_array($data)) {
+
+
+                            ?>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <div class="flex-shrink-0 h-10 w-10">
+                                                <img class="h-10 w-10 rounded-full"
+                                                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                                                    alt="">
+                                            </div>
+                                            <div class="ml-4">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <?php echo $d['nama']; ?>
+                                                </div>
+                                                <div class="text-sm text-gray-500">
+                                                    <?php echo $d['email']; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-900"><?php echo $d['jenis_ptk']; ?></div>
+                                        <div class="text-sm text-gray-500"><?php echo $d['tugas_tambahan']; ?></div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-900"><?php echo $d['tempat_lahir']; ?></div>
+                                        <div class="text-sm text-gray-500"><?php echo $d['tanggal_lahir']; ?></div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <?php echo $d['noHp']; ?>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Details</a>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="index.js"></script>
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -54,8 +54,9 @@ include 'koneksi.php';
         </div>
         <!-- content -->
         <div class="flex-1 p-10 text-2xl font-bold">
-            main content
+            konten
         </div>
+
     </div>
     <script src="index.js"></script>
 </body>
